3a7ae0a01eb73208a8b77f2de083bdcee13e9bd9
Your Name <you@example.com>
2021-08-30 14:40:33 +0800

    前期准备

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+// import app from ''
+export default {
+  name:'App',
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+// import App from '@/App'
+// import App from '@/'
+// import app from '@/components/HelloWorld.vue'
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
